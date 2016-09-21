7a192470a5abd7214d94acb90b3351b12303fbbc
Sebastien Le Marchand <sebastien.lemarchand@gmail.com>
2016-09-21 21:14:33 +0200

    Improve some test structure

--- test/acceptance/format-test.js
+++ test/acceptance/format-test.js
@@ -2,24 +2,30 @@ var expect = require("chai").expect;
 
 var fse = require('fs-extra');
 
+var tmp = require('tmp');
+
 var main = require('../../main');
+
+var base; 
+
+before(function() {
+
+    base = tmp.dirSync().name;
+
+    fse.copySync('test/acceptance/data/format-test', base);
+});
+
 
 describe("Format Command", function() {
   it("format properties file", function() {
 
-    var base = 'test/acceptance/data/format-test';
-
     var inputFilePath =  base + '/format_input.properties';
     
-    var actualFilePath = base + '/format_actual.properties';
-
     var expectedFilePath = base + '/format_expected.properties';
 
-    fse.copySync(inputFilePath, actualFilePath);
+    main.run(['format', inputFilePath]);
 
-    main.run(['format', actualFilePath]);
-
-    var actualContent = fse.readFileSync(actualFilePath, 'utf8').toString();
+    var actualContent = fse.readFileSync(inputFilePath, 'utf8').toString();
 
     var expectedContent = fse.readFileSync(expectedFilePath, 'utf8').toString();
 
