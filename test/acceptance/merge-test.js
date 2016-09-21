33d66d9921cdc66a16610ad0e28dda2c95475d20
Sebastien Le Marchand <sebastien.lemarchand@gmail.com>
2016-09-22 00:11:03 +0200

    Refactor merge test

--- test/acceptance/merge-test.js
+++ test/acceptance/merge-test.js
@@ -2,26 +2,31 @@ var expect = require("chai").expect;
 
 var fse = require('fs-extra');
 
+var tmp = require('tmp');
+
 var main = require('../../main');
 
-describe("Merge Command", function() {
-  it("merge properties files", function() {
+var base; 
 
-    var base = 'test/acceptance/data/merge-test';
+before(function() {
+
+    base = tmp.dirSync().name;
+
+    fse.copySync('test/acceptance/data/merge-test', base);
+});
+
+describe("props merge", function() {
+  it("merge a properties file into another properties file", function() {
 
     var fromFilePath =  base + '/merge_from.properties';
     
     var intoFilePath =  base + '/merge_into.properties';
 
-    var actualFilePath = base + '/merge_actual.properties';
-
     var expectedFilePath = base + '/merge_expected.properties';
 
-    fse.copySync(intoFilePath, actualFilePath);
+    main.run(['merge', fromFilePath, intoFilePath]);
 
-    main.run(['merge', fromFilePath, actualFilePath]);
-
-    var actualContent = fse.readFileSync(actualFilePath, 'utf8').toString();
+    var actualContent = fse.readFileSync(intoFilePath, 'utf8').toString();
 
     var expectedContent = fse.readFileSync(expectedFilePath, 'utf8').toString();
 
